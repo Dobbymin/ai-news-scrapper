444b85be94464fa21834e16b05f85a1d091e6dd5
Dobbymin <gmin990624@gmail.com>
2025-11-01 13:39:38 +0900

    init: 전체 프로젝트 구조 생성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,57 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-plugin-prettier";
+import unusedImports from "eslint-plugin-unused-imports";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+const eslintConfig = [
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "public/mockServiceWorker.js", "next-env.d.ts"],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:prettier/recommended"),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      "jsx-a11y": jsxA11y,
+      prettier,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/jsx-sort-props": [
+        "warn",
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+        },
+      ],
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/click-events-have-key-events": "warn",
+      "jsx-a11y/no-static-element-interactions": "warn",
+      "unused-imports/no-unused-imports": "warn",
+      "prettier/prettier": ["warn", { usePrettierrc: false }],
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+      react: {
+        version: "19.2.0",
+      },
+    },
+  },
+];
 
 export default eslintConfig;
